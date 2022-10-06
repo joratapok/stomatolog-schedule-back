88f4408673f67cc155e8f65a0b79f214927825ad
Paul <twin>
2022-10-06 21:10:10 +0700

    Update urls for Clinic model

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('/api/auth/sign_in/'),
     # # Авторизация
     # path('/api/auth/sign_up/'),
-    path('api/auth/', include('rest_framework.urls')),
-    path('api/events/', views.EventListApiView.as_view(), name='event_list'),
-    path('api/events/<int:pk>/', views.EventDetailApiView.as_view(), name='event_detail'),
+    # path('api/auth/', include('rest_framework.urls')),
+    path('api/events/', views.ClinicListApiView.as_view(), name='clinic_list'),
+    path('api/events/<int:pk>/', views.ClinicDetailApiView.as_view(), name='clinic_detail'),
 ]
