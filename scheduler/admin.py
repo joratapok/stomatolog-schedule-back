fc82d4656695d203c3aa0052df60d282f624ebd5
Paul <twinencrypt@gmail.com>
2022-11-09 11:47:03 +0700

    added class ClinicAdmin

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Customer, Clinic, Cabinet, Event, Profile
 
+
+class ClinicAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+
+
+admin.site.register(Clinic, ClinicAdmin)
 admin.site.register(Profile)
 admin.site.register(Customer)
-admin.site.register(Clinic)
 admin.site.register(Cabinet)
 admin.site.register(Event)
+
+
