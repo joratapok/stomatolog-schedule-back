0005a4267776a494486e6149a42f4ea5bbe248fa
Paul <twin>
2023-01-02 17:59:20 +0700

    Change filter query

--- price/views.py
+++ price/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.postgres.search import TrigramSimilarity
 from price.models import PriceList, Service
 from price.serializers import PriceListSerializer, ServiceSerializer
 from scheduler.permissions import IsOwnerOrAdministrator
@@ -21,9 +22,16 @@ class ServiceListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ServiceSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrAdministrator]
 
+    # def get_queryset(self):
+    #     if 'service' in self.request.query_params:
+    #         return Service.objects.filter(title__icontains=self.request.query_params['service'])[:10]
+    #     return Service.objects.all()[:10]
+
     def get_queryset(self):
         if 'service' in self.request.query_params:
-            return Service.objects.filter(title__icontains=self.request.query_params['service'])[:10]
+            return Service.objects.annotate(
+                similarity=TrigramSimilarity('title', self.request.query_params['service']),
+                ).filter(similarity__gt=0.1)[:10]
         return Service.objects.all()[:10]
 
 
