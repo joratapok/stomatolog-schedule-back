098f96e82692d2bece0e02f4f1d2d0eaff9daac1
Paul <twinencrypt@gmail.com>
2023-03-03 16:49:23 +0700

    Add signal to Employee model

--- employee/signal.py
+++ employee/signal.py
@@ -7,4 +7,4 @@ from django.contrib.auth.models import User
 def create_customer_dental_chart(sender, instance, created, **kwargs):
     if created:
         if User.objects.count() == 1:
-            User.objects.filter(id=instance.id).update(last_name='admin', first_name='admin')
+            User.objects.filter(id=instance.id).update(last_name='admin', first_name='admin', profile__role='owner')
