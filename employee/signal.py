7779c7ec31b6767cb1f4fa89fadc8e35af27f774
Paul <twin>
2023-03-05 17:16:10 +0700

    Rename method

--- employee/signal.py
+++ employee/signal.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 @receiver(post_save, sender=User)
-def create_customer_dental_chart(sender, instance, created, **kwargs):
+def create_user_admin_fields(sender, instance, created, **kwargs):
     if created:
         if User.objects.count() == 1:
             User.objects.filter(id=instance.id).update(last_name='admin', first_name='admin')
