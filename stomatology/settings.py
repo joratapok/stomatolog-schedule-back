00eb0e74859be4ffabb031d7704960459bcf6156
Paul <twinencrypt@gmail.com>
2023-03-16 11:39:10 +0700

    Add CSRF_TRUSTED_ORIGINS parameter

--- stomatology/settings.py
+++ stomatology/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
+
 # Application definition
 
 INSTALLED_APPS = [
